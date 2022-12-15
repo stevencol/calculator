263f07492270f4bee4c6143edd769ba3238e819b
stevencol <steven2088hotmail.es>
2022-12-14 20:03:35 -0500

    se crea logica, la calculadora es funcional pero solo reliza operaciones de manera consecutiva

--- js/calculator.js
+++ js/calculator.js
@@ -1,82 +1,102 @@
-
-
-alert(2);
 //buttons value
 let display = document.getElementById("display");
-let value1="";
-let value2="";
-let operator="";
+let value1 = "";
+let value2 = null;
+let value3 = 0;
+let lastOperator = "";
 
 const addDisplay = (number) => {
-   
-    if(value1.length <11){
-        display.innerHTML ="";
-        value1 = value1 + number;
-        display.append(value1);
-        console.log(value1)
-    }
+  console.log(number);
+  if (value1 < 99999999999) {
+    display.innerHTML = "";
+    value1 = Number(value1 + "" + number);
+    console.log(value1);
+    display.append(value1);
 
-}
-const deleteDigit=() => {
-
-    value1 = value1.substring(0 , value1.length-1);
-    display.innerHTML = value1;
-    if(value1.length<1){
-        display.innerHTML = "."; 
-    }
-}
-
-const operate = (operator) => {
- this.operator = operator;
-
- 
-
-  switch (operator){
-    case "+": 
-     if(value2==""){
-       value2 = value1;
-       value1="";
-       display.innerHTML="ss";
-     }else{
-        value1 =   parseInt(value2) +parseInt(value1) ;
-        value2="";
-        display.innerHTML=value1+"";
-     }
-
-    break;
-
-    case "-": 
-     if(value2==""){
-       value2 = value1;
-       value1="";
-       display.innerHTML=".";
-     }else{
-        value1 =   parseInt(value2) -parseInt(value1);
-        console.log(value1);
-        value2="";
-        display.innerHTML= value1+"";
-     }
-
-    break;
- 
-
-  
+    console.log(String(value1));
   }
- 
-}
+};
+const deleteDigit = () => {
+  value1 = String(value1);
+  value1 = value1.substring(0, value1.length - 1);
+  value1 = Number(value1);
+  console.log(value1);
+  display.innerHTML = value1;
+  if (value1.length < 1) {
+    display.innerHTML = "0";
+  }
+};
 
-const result =()=>{
-    switch (operator){
-        case "+":
-            display.innerHTML =parseInt(value1) + parseInt(value2);
-            console.log(parseInt(value1) + parseInt(value2));
-            break;
-    }
-}
+const operate = (operate) => {
+  switch (operate) {
+    case "+":
+      if (value2 == null) {
+        value2 = value1;
+        console.log(value2);
+        value1 = 0;
+      } else {
+        console.log(value1, value2);
 
+        value3 = value2 + value1;
+        value2 = value3;
+        display.innerHTML = value3;
 
+        value1 = 0;
+      }
 
+      break;
+    case "-":
+      if (value2 == null) {
+        value2 = value1;
+        console.log(value2 + "si endrteo");
+        value1 = 0;
+      } else {
+        console.log(value1, value2);
 
+        value3 = value2 - value1;
+        value2 = value3;
+        display.innerHTML = value3;
+        console.log(value2);
+        value1 = 0;
 
+      }
+      break;
+    case "*":
+      if (value2 == null) {
+        value2 = value1;
+        console.log(value2 + "si endrteo");
+        value1 = 0;
+      } else {
+        console.log(value1, value2);
 
+        value3 = value2 * value1;
+        value2 = value3;
+        display.innerHTML = value3;
+        console.log(value2);
+        value1 = 0;
 
+      }
+
+      break;
+      case "/":
+      if (value2 == null) {
+        value2 = value1;
+        console.log(value2 + "si endrteo");
+        value1 = 0;
+      } else {
+        console.log(value1, value2);
+
+        value3 = value2 / value1;
+        value2 = value3;
+        display.innerHTML = value3;
+        console.log(value2);
+        value1 = 0;
+
+      }
+
+      break;
+
+    default:
+      break;
+  }
+};
