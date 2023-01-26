e148a6046bd7c0bef1cf3d479e954d2dd2999eb2
stevencol <steven2088hotmail.es>
2023-01-26 15:38:19 -0500

    Se modican archivos que causaron mal funcionamineto

--- js/calculator.js
+++ js/calculator.js
@@ -29,27 +29,28 @@ const deleteDigit = () => {
   }
 };
 
-
 const checkLastOperation = (currentOperator) => {
-
-  if (resultado == null && value2 != null && lastOperator=="") {
+  if (resultado == null && value2 != null && lastOperator == "") {
     value2 = value1;
     value1 = 0;
-    lastOperator=currentOperator;
-    console.log("entro y gaurda el valor 1 en en 2 y guarda operador  y lo borra de la variable principal" + " valor 2: " +value2 +"Operador:; " + lastOperator) ;
-    
+    lastOperator = currentOperator;
+    console.log(
+      "entro y gaurda el valor 1 en en 2 y guarda operador  y lo borra de la variable principal" +
+        " valor 2: " +
+        value2 +
+        "Operador:; " +
+        lastOperator
+    );
+
     currentOperator = "";
     console.log("1");
-  }
+  } else if ((lastOperator != "=" && lastOperator != "") || value2 != null) {
+    console.log(" opera con el anterio que es" + lastOperator);
 
-  else if (lastOperator != "=" && lastOperator != "" || value2!=null) {
-   
-  console.log(" opera con el anterio que es" + lastOperator);
- 
     operate(lastOperator);
-    lastOperator= currentOperator=="="? "":currentOperator; 
-   
-    currentOperator="";
+    lastOperator = currentOperator == "=" ? "" : currentOperator;
+
+    currentOperator = "";
     console.log(" guarda en nuevo borerador  y borra el viejo" + lastOperator);
     value2 = resultado;
     value1 = 0;
@@ -57,25 +58,18 @@ const checkLastOperation = (currentOperator) => {
     console.log("2");
   }
 
-  if(currentOperator=="="){
+  if (currentOperator == "=") {
     operate(lastOperator);
-    value2=resultado;
-     value1 = 0;
+    value2 = resultado;
+    value1 = 0;
     display.innerHTML = resultado;
     console.log(value2);
     console.log("curente");
   }
-
-
-
-
 };
-
-
 
 const addition = () => {
   return value2 + value1;
-
 };
 
 const subtraction = () => {
@@ -90,21 +84,14 @@ const division = (operator) => {
   return value2 / value1;
 };
 
-
 const result = (operator) => {
-
-  operate(lastOperator, this.value1 , this.value2);
- 
-
-  
+  operate(lastOperator, this.value1, this.value2);
 };
-
 
 const operate = (operator, value1, value2) => {
   switch (operator) {
     case "+":
       resultado = addition(this.value1, this.value2);
-
 
       break;
     case "-":
